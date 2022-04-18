cd3512e63c2216190617d80ac213028dbf7038f7
Jaret Balba <Jaretzbalba@gmail.com>
2022-04-18 14:15:18 -0700

    update js to show all movie options based on search input

--- js/main.js
+++ js/main.js
@@ -11,11 +11,41 @@ function getFetch(){
       .then(data => {
 
         console.log(data.results)
+
+        const item = new MovieInfo(data) 
+        item.showResults()
+
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
 
+}
+
+//Create the class outside of the function and CALL it within the function
+class MovieInfo {
+  constructor(movieData) { // I am passing in data
+    this.results = movieData.results
+  }
+
+  showResults() {
+    let tableRef = document.getElementById('movie-table')
+
+    //No i++ because you are deleting the row so i is always the next row
+    for( let i = 1; i < tableRef.rows.length;) { 
+      tableRef.deleteRow(i);
+    }
+    for(let key in this.results) {
+      let newRow = tableRef.insertRow(-1)
+      let newImageCell = newRow.insertCell(0)
+      let newTitleCell = newRow.insertCell(1)
+      let newImage = document.createElement('img')
+      newImage.src = this.results[key].image
+      let newTitleText = document.createTextNode(this.results[key].title)
+      newImageCell.appendChild(newImage)
+      newTitleCell.appendChild(newTitleText)
+    } 
+  }
 }
 
 
